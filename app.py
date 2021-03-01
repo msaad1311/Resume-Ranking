66171f2d6bc46c71db71429ad5112f9b3b1a408d
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-03-01 10:43:00 -0500

    removed the elliot resume and updated the code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
-import main
+from flask import Flask,render_template,request,redirect,url_for
+import main as an
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -8,7 +9,15 @@ def home():
     return render_template('index.html')
 @app.route('/analysis',methods=['POST','GET'])
 def analysis():
+    tempCV = request.files['cv']
+    tempCV.save('tempCV.pdf')
+    
+    
+    # lines =[line.decode('utf-8') for line in uploadedCV]
+    # print(lines)
     return 'hello world'
+    # cvsText = an.cleaner(an.pdfReader(cvs))
+    # jobText = an.cleaner(an.pdfReader(job))
 
 if __name__ == '__main__':
     app.run(debug=True)
